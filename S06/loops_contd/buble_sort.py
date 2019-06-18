c68828ff62cf091892cf4979f53c86592090ef56
iamvee <naeini.v@gmail.com>
2019-06-18 09:21:26 -0700

    files

---
+++ S06/loops_contd/buble_sort.py
@@ -0,0 +1,17 @@
+lst = [210, 12, 33, 4, 70, 6]
+# print(lst)
+
+while True:
+    lst_old = lst.copy()
+    for i in range(len(lst) - 1):
+        if lst[i] > lst[i + 1]:
+            lst[i], lst[i + 1] = lst[i+1], lst[i]
+
+    if :
+        break
+    print(lst)
+
+
+
+
+
--- S06/loops_contd/prime_number.py
+++ S06/loops_contd/prime_number.py
@@ -1,2 +1,10 @@
-number = 101
+number = 1010
+
+
+for i in range(2, number):
+    if number % i == 0:
+        print("is not prime")
+        break
+else:
+     print("prime")
 
