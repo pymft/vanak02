326ee510fa512d121b3f59fea8f8cd6752fae8a2
iamvee <naeini.v@gmail.com>
2019-06-25 08:24:32 -0700

    fibonacci solution

---
+++ S08/recursive/fibonacci.py
@@ -0,0 +1,12 @@
+def fib(n):
+    if n in (0, 1):
+        return n
+    return fib(n-1) + fib(n-2)
+
+
+if __name__ == '__main__':
+    for i in range(20):
+        print(fib(i), end=' ')
+
+    # OUTPUT:
+    # 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181
