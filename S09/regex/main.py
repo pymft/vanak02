17c77b1bae25997d292bb570c0aca370506fab83
iamvee <naeini.v@gmail.com>
2019-06-30 09:23:30 -0700

    regex

---
+++ S09/regex/main.py
@@ -0,0 +1,21 @@
+import re
+import urllib.request
+
+def read_from_site(url):
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+    fp = urllib.request.urlopen(req)
+    mybytes = fp.read()
+
+    mystr = mybytes.decode("utf8")
+    fp.close()
+
+    return mystr
+
+
+
+pat = r"\b\w+@[\w\.]+\b"
+text = read_from_site("https://www.mtu.edu/mechanical/people/faculty-staff/")
+
+res = re.findall(pat, text)
+print(res)
