a42855190570e587a3610e286b83683c4a2ebdcf
iamvee <naeini.v@gmail.com>
2019-07-02 08:54:06 -0700

    log without decorator

--- S10/decorators/question.py
+++ S10/decorators/question.py
@@ -1,11 +1,27 @@
-@log
+from time import ctime
+
+
+
 def factorial(n):
     fact = 1
-    while n > 0:
-        fact *= n
-        n -= 1
+    i = 1
+    while i <= n:
+        fact *= i
+        i += 1
+    f = open('output.log', mode='a')
+    f.write(ctime() + " | fact | " + str(n) + " | " + str(fact) + "\n")
+    f.close()
     return fact
 
+def mysum(a, b):
+    out = a + b
+    f = open('output.log', mode='a')
+    f.write(ctime() + " | mysum | " + str((a, b)) + " | " + str(out) + "\n")
+    f.close()
+    return out
 
 
-factorial(100)
+factorial(10)
+mysum(10, 20)
+factorial(2)
+factorial(5)
