fdcc12a6a500f60625bf798a493bdf8e83d0af90
iamvee <naeini.v@gmail.com>
2019-07-09 06:12:00 -0700

    vector class , add_vectors function

---
+++ S12/oop-2/vector.py
@@ -0,0 +1,12 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+def add_vectors(a, b):
+    # complete me
+
+v1 = Vector(3, 4)
+v2 = Vector(10, 1)
+v3 = add_vectors(v1, v2)
