389611923873c1e9e85155241284d042b3af9d72
iamvee <naeini.v@gmail.com>
2019-07-09 06:42:47 -0700

    __sub__ and __neg__

--- S12/oop-2/vector.py
+++ S12/oop-2/vector.py
@@ -3,16 +3,20 @@ class Vector:
         self.x = x
         self.y = y
 
-    def add(self, b):
-        x_new = self.x + b.x
-        y_new = self.y + b.y
+    def __add__(self, other):
+        x_new = self.x + other.x
+        y_new = self.y + other.y
 
         return Vector(x_new, y_new)
+
+    def __sub__(self, other):
+        pass
+
+    def __neg__(self):
+        pass
 
 
 v1 = Vector(3, 4)
 v2 = Vector(10, 1)
-v3 = v1.add(v2)
-v4 = v1.add(v3)
-v6 = v4.add(v3)
+v3 = v1 + v2 + v1 + v2 + v2
 print(v3.x, v3.y)
