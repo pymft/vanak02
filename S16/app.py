fce5bb9003a6f0f625c86c2e37f3e0be2cdcedc0
iamvee <naeini.v@gmail.com>
2019-07-23 07:45:21 -0700

    only index function

--- S16/app.py
+++ S16/app.py
@@ -10,18 +10,6 @@ def index(rows=100, cols=100):
     return render_template("index.html", rows=rows, cols=cols)
 
 
-@app.route('/login')
-def login():
-    with open("./templates/login.html") as f:
-        text = f.read()
-    return text
-
-
-@app.route("/greeting/<name>")
-def greeting(name):
-    with open("./templates/greeting.html") as f:
-        text = f.read()
-    return text.format(name=name)
 
 
 app.run(port=8080)
